fc6049aa5d6dea38c0c6bc4db3f088464127b651
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2023-09-05 12:25:20 +0200

    admin login

--- klice/admin.go
+++ klice/admin.go
@@ -5,4 +5,15 @@ import (
 )
 
 func handleAdmin(writer http.ResponseWriter, req *http.Request) {
+	user, pass, ok := req.BasicAuth()
+	if ok {
+		password, err := rdb.Get(ctx, "admin/user/"+user).Result()
+		if err == nil && pass == password {
+			writer.Write([]byte("ahoj"))
+			return
+		}
+	}
+	writer.Header().Add("WWW-Authenticate", `Basic realm="Přihlaš se!"`)
+	writer.WriteHeader(http.StatusUnauthorized)
+	return
 }
